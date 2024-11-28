06e024b49720641c6973a0ecf7ca270841688a0e
guibas00 <guibashenri@gmail.com>
2024-11-28 20:59:47 -0300

    connect with database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const qrcode = require('qrcode');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const {v4} = require('uuid')
 
 const app = express();
 const port = 5000;
@@ -9,7 +11,22 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-// Configuração do Swagger (opcional)
+// Conexão com o MongoDB
+const mongoURI = `mongodb://mongo:iRyYMmxslKQLNRszYTixmowIkkUEmFgX@junction.proxy.rlwy.net:30044`;
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch(err => console.error('Erro ao conectar ao MongoDB:', err));
+
+// Schema do MongoDB para os QR Codes
+const qrCodeSchema = new mongoose.Schema({
+  cpf: { type: String, required: true, unique: false },
+  uuid: {type: String, required: false, unique: true},
+  qrCodeData: { type: String, required: true, unique: true },
+  image: { type: String, require: false, unique: false}
+});
+
+const QRCode = mongoose.model('pass', qrCodeSchema);
+
 // Configuração do Swagger
 const swaggerDocument = {
   openapi: '3.0.0',
@@ -34,8 +51,9 @@ const swaggerDocument = {
                   data: { type: 'string' },
                   hora: { type: 'string' },
                   local: { type: 'string' },
+                  cpf: { type: 'string' },
                 },
-                required: ['nome', 'evento', 'data', 'hora', 'local'],
+                required: ['nome', 'evento', 'data', 'hora', 'local', 'cpf'],
               },
             },
           },
@@ -70,9 +88,9 @@ const swaggerDocument = {
               schema: {
                 type: 'object',
                 properties: {
-                  qrCodeData: { type: 'string' },
+                  cpf: { type: 'string' },
                 },
-                required: ['qrCodeData'],
+                required: ['cpf'],
               },
             },
           },
@@ -90,36 +108,88 @@ const swaggerDocument = {
         },
       },
     },
+    // ---->>>> AQUI ESTÁ A PARTE DO GET <<<<----
+    '/ingressos/{cpf}': { 
+      get: {
+        summary: 'Consulta ingressos por CPF',
+        parameters: [
+          {
+            in: 'path',
+            name: 'cpf',
+            schema: {
+              type: 'string',
+            },
+            required: true,
+            description: 'CPF do usuário',
+          },
+        ],
+        responses: {
+          '200': {
+            description: 'Ingressos encontrados',
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      Nome: { type: 'string' },
+                      Evento: { type: 'string' },
+                      Data: { type: 'string' },
+                      Hora: { type: 'string' },
+                      Local: { type: 'string' },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          '404': {
+            description: 'Nenhum ingresso encontrado',
+          },
+          '500': {
+            description: 'Erro ao consultar ingressos',
+          },
+        },
+      },
+    },
   },
 };
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Função para gerar QR code
-function gerarQrCode(dados) {
+// Função para gerar QR code (modificada para salvar no banco)
+async function gerarQrCode(dados, cpf, uuid, object) {
   return new Promise((resolve, reject) => {
-    qrcode.toDataURL(dados, {
-      version: 4,
+    qrcode.toDataURL(JSON.stringify(object), {
+      version: 5,
       errorCorrectionLevel: 'L',
-      width: 200, // Ajuste o tamanho conforme necessário
+      width: 100,
       margin: 4,
-    }, (err, url) => {
+    }, async (err, url) => {
       if (err) {
         reject(err);
       } else {
-        resolve(url);
+        try {
+          // Salva o QR code no banco de dados
+          const novoQRCode = new QRCode({ cpf, uuid, qrCodeData: dados, image: url  });
+          await novoQRCode.save();
+          resolve(url);
+        } catch (error) {
+          reject(error);
+        }
       }
     });
   });
 }
 
-// Função para gerar ingresso
+// Função para gerar ingresso (modificada para incluir CPF)
 app.post('/gerar_ingresso', async (req, res) => {
   try {
-    const { nome, evento, data, hora, local } = req.body;
-
+    const { nome, evento, data, hora, local, cpf } = req.body;
+    const uuidQr = v4()
     // Gerar dados do QR code
     const dadosQr = `Nome: ${nome}\nEvento: ${evento}\nData: ${data}\nHora: ${hora}\nLocal: ${local}`;
-    const qrCodeUrl = await gerarQrCode(dadosQr);
+    const qrCodeUrl = await gerarQrCode(dadosQr, cpf, uuidQr, {cpf, uuidQr});
 
     // Retornar imagem do QR code como JSON
     res.json({ qrCodeUrl });
@@ -129,12 +199,58 @@ app.post('/gerar_ingresso', async (req, res) => {
   }
 });
 
-// Função para validar ingresso (ainda precisa ser implementada)
-app.post('/validar_ingresso', (req, res) => {
-  // Implementar lógica de validação do QR code aqui
-  // ...
-  res.json({ status: 'valido' });
+// Função para validar ingresso (busca pelo CPF)
+app.post('/validar_ingresso', async (req, res) => {
+  try {
+    const { cpf, uuid } = req.body;
+
+    // Encontra o QR code no banco de dados pelo CPF
+    const qrCode = await QRCode.findOne({ cpf, uuid });
+
+    if (qrCode) {
+      res.json({ status: 'válido', qrCodeData: qrCode.image });
+    } else {
+      res.status(400).json({ status: 'inválido', message: 'CPF não encontrado' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao validar ingresso.');
+  }
 });
+
+// Endpoint para consultar ingressos por CPF
+app.get('/ingressos/:cpf', async (req, res) => {
+  try {
+    const cpf = req.params.cpf;
+
+    // Encontra os QR codes no banco de dados pelo CPF
+    const qrCodes = await QRCode.find({ cpf });
+
+    if (qrCodes.length > 0) {
+      // Formata a resposta para incluir apenas os dados relevantes
+      const ingressos = qrCodes.map(qrCode => {
+        const dados = qrCode.qrCodeData.split('\n');
+        const image = qrCode.image;
+        const ingresso = {};
+        dados.forEach(linha => {
+          const [chave, valor] = linha.split(': ');
+          ingresso[chave.trim()] = valor.trim();
+          ingresso.image = image
+        });
+        return ingresso;
+      });
+      res.json(ingressos);
+    } else {
+      res.status(404).json({ message: 'Nenhum ingresso encontrado para este CPF' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao consultar ingressos.');
+  }
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
