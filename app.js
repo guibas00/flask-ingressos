fcdd88fc0de5a1c5cb37a8e6fe14110458c9f48d
guibas00 <guibashenri@gmail.com>
2024-11-28 22:18:43 -0300

    fix log

--- app.js
+++ app.js
@@ -204,6 +204,7 @@ app.post('/validar_ingresso', async (req, res) => {
   try {
     const { cpf, uuidQr } = req.body;
     const uuid = uuidQr
+    console.log(`Validando com o corpo ${req.body}`)
     // Encontra o QR code no banco de dados pelo CPF
     const qrCode = await QRCode.findOne({ cpf, uuid });
 
